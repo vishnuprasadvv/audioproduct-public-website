b1f44af485609c8ab2c19d7ab43d0666cae8a4cb
Vishnu prasad V V <vishnuprasadvv24@gamil.comgit config --global user.name vishnuprasadvv>
2024-02-15 20:30:49 +0530

    audio

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('#okbutton').click(function(){
+        $('#popup').hide();
+    })
+    $('#submit-btn').submit(function(){
+        $('#popup').show();
+    })
+    
+})
+
